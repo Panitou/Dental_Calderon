7c9d1187b2a34e55ffdec42455c5567e66b4a953
Panitou <joaco0mr4@gmail.com>
2024-01-04 01:21:44 -0500

    Cambio de url

--- src/main/java/proyecto/clinica_dental_calderon/DB/Conexion.java
+++ src/main/java/proyecto/clinica_dental_calderon/DB/Conexion.java
@@ -9,9 +9,13 @@ public class Conexion {
 
     private static int connectionCount = 0;
 
-    private static final String URL = "jdbc:mysql://database-calderon-clinica.cxoo1zsccafn.sa-east-1.rds.amazonaws.com:3306/Clinica_Calderon_DB";
-    private static final String USER = "admin";
-    private static final String PASSWORD = "Kmm-Nt?cSy4eX9Jj#ThJ";
+//    private static final String URL = "jdbc:mysql://database-calderon-clinica.cxoo1zsccafn.sa-east-1.rds.amazonaws.com:3306/Clinica_Calderon_DB";
+//    private static final String USER = "admin";
+//    private static final String PASSWORD = "Kmm-Nt?cSy4eX9Jj#ThJ";
+    
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/dental_calderon";
+    private static final String USER = "root";
+    private static final String PASSWORD = "qle6vzZhgnBqfCINGXjG";
 
     public static Connection getConnection() {
         Connection connection = null;
